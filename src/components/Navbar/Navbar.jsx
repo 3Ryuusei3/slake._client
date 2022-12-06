9e9a58bdfbdc70f493233f47e0d34da4829f0e74
Manuel Atance <ryuusei3@MacBook-Pro-de-Manuel.local>
2022-12-06 11:22:31 +0100

    Colors palette changed and improved Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,28 +6,31 @@ function Navigation() {
 	return (
 		<>
 			<Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-				<Container>
+				<Container className="py-2">
 					<Navbar.Brand>
-						<Link to="/" className="brand-link">
+						<Link to="/" className="brand-link py-2 px-3">
+							<img className="nav-icon me-3 mb-1" src="https://res.cloudinary.com/dhws4e2ty/image/upload/v1670318446/favicon_rpvk1o.ico" alt="" />
 							slake.
 						</Link>
 					</Navbar.Brand>
 					<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 					<Navbar.Collapse id="responsive-navbar-nav">
 						<Nav className="me-auto">
-							<Link to="/pricing" className="nav-link">
+							<Link to="/pricing" className="nav-btn">
 								<Nav.Link as="div">Pricing</Nav.Link>
 							</Link>
-							<Link to="/about" className="nav-link">
+							<Link to="/about" className="nav-btn">
 								<Nav.Link as="div">About</Nav.Link>
 							</Link>
 						</Nav>
 						<Nav>
-							<Link to="/login" className="nav-link">
+							<Link to="/login" className="nav-btn">
 								<Nav.Link as="div">Log In</Nav.Link>
 							</Link>
-							<Link to="/signup" className="red-btn" style={{ maxWidth: "max-content" }}>
-								<Nav.Link as="div">Try slake. for free</Nav.Link>
+							<Link to="/signup" className="ms-3 purple-btn" style={{ maxWidth: "max-content" }}>
+								<Nav.Link as="div" className="px-3 text-light">
+									Try slake. for free
+								</Nav.Link>
 							</Link>
 						</Nav>
 					</Navbar.Collapse>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import { Container, Row, Col, Nav } from "react-bootstrap"
+import { Container, Row, Col } from "react-bootstrap"
 import { Link } from "react-router-dom"
 
 function Homepage() {
@@ -6,16 +6,14 @@ function Homepage() {
 		<Container className="mt-5">
 			<Row className="mt-5">
 				<Col className="text-center">
-					<img src="https://www.notion.so/cdn-cgi/image/format=auto,width=640,quality=100/front-static/pages/product/home-page-hero-refreshed-v3.png" alt="Team" />
+					<img src="https://res.cloudinary.com/dhws4e2ty/image/upload/v1670317681/undraw_experience_design_re_ca7l_bgrfu1.svg" alt="Team" />
 					<h1 className="mt-5">One place.</h1>
 					<h1>All your data.</h1>
 					<h1>Just for you.</h1>
-					<h4 className="pt-4">Create new notes and share your notes, </h4>
+					<h4 className="pt-4">Create new notes and share them, </h4>
 					<h4>play with the kanban or finish your to-do list. </h4>
-					<Link to="/signup" className="red-btn" style={{ maxWidth: "max-content", marginInline: "auto" }}>
-						<Nav.Link as="div" className="mt-4">
-							Try slake. for free
-						</Nav.Link>
+					<Link to="/signup" className="purple-btn my-5 px-5 py-2" style={{ maxWidth: "max-content", marginInline: "auto" }}>
+						Try slake. for free
 					</Link>
 				</Col>
 			</Row>
