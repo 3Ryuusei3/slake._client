463b7522bd23b3b8622e266347f967f02b2c7128
Alberto Naval <albertonaval.dev@gmail.com>
2022-12-14 18:32:17 +0100

    Stripe

--- src/components/CheckoutForm/CheckoutForm.jsx
+++ src/components/CheckoutForm/CheckoutForm.jsx
@@ -1,17 +1,16 @@
 import { useStripe, useElements, PaymentElement } from '@stripe/react-stripe-js';
 
 const CheckoutForm = () => {
+
     const stripe = useStripe();
     const elements = useElements();
 
     const handleSubmit = async (event) => {
-        // We don't want to let default form submission happen here,
-        // which would refresh the page.
+
         event.preventDefault();
 
         if (!stripe || !elements) {
-            // Stripe.js has not yet loaded.
-            // Make sure to disable form submission until Stripe.js has loaded.
+
             return;
         }
 
