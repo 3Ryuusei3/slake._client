bf0b454af582555822187f000fcf42471d71839b
Manuel Atance <ryuusei3@MacBook-Pro-de-Manuel.local>
2022-12-31 12:03:59 +0100

    Sidebar state now correctly saves if open or not

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -29,7 +29,11 @@ const Sidebar = () => {
 	return (
 		<>
 			<div className="sidebar" style={{ display: "flex", height: "100vh", overflow: "scroll initial" }}>
-				<CDBSidebar style={!darkMode ? { color: "var(--text-primary)", backgroundColor: "var(--bg-navbar)" } : { color: "var(--dark-text-primary)", backgroundColor: "var(--dark-bg-navbar)" }}>
+				<CDBSidebar
+					toggled={!isSidebarOpen ? true : false}
+					breakpoint={600}
+					style={!darkMode ? { color: "var(--text-primary)", backgroundColor: "var(--bg-navbar)" } : { color: "var(--dark-text-primary)", backgroundColor: "var(--dark-bg-navbar)" }}
+				>
 					<CDBSidebarHeader
 						prefix={
 							<img
