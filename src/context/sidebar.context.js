d573df7f65aa94e799900cccab24f85f43e31e80
Manuel Atance <ryuusei3@MacBook-Pro-de-Manuel.local>
2022-12-31 11:39:56 +0100

    Fixed sidebar bug

--- src/context/sidebar.context.js
+++ src/context/sidebar.context.js
@@ -3,7 +3,7 @@ import { createContext, useState } from "react"
 const SidebarContext = createContext()
 
 const SidebarProviderWrapper = props => {
-	const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+	const [isSidebarOpen, setIsSidebarOpen] = useState(true)
 
 	return <SidebarContext.Provider value={{ isSidebarOpen, setIsSidebarOpen }}>{props.children}</SidebarContext.Provider>
 }
