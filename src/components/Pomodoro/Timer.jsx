bb84f784539c7a338bfd08a4c3e012ecbd8a6461
Manuel Atance <atance3@hotmail.com>
2023-01-04 14:36:42 +0100

    Improved styling for Pomodoro and small bugfixes on notes print

--- src/components/Pomodoro/Timer.jsx
+++ src/components/Pomodoro/Timer.jsx
@@ -1,133 +1,136 @@
-
 import { useContext, useState, useEffect, useRef } from "react"
 
 import { DarkModeContext } from "../../context/darkmode.context"
 import { SidebarContext } from "../../context/sidebar.context"
 import { PomodoroContext } from "../../context/pomodoro.context"
 
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar"
 import toast from "react-hot-toast"
 
-import PlayButton from "./PlayButton"
-import PauseButton from "./PauseButton"
-import SettingsButton from "./SettingButton"
-
-const gray = '#8f8e8a'
-const purple = '#6c63ff'
-const lightGray = '#'
-
-
 const Timer = () => {
+	const { darkMode } = useContext(DarkModeContext)
+	const { setShowSettings, workMinutes, breakMinutes } = useContext(PomodoroContext)
 
+	const [isPaused, setIsPaused] = useState(true)
+	const [mode, setMode] = useState("work")
+	const [secondsLeft, setSecondsLeft] = useState(0)
+	const [showSettingIcon, setShowSettingIcon] = useState(false)
 
-    const { darkMode } = useContext(DarkModeContext)
-    const { setShowSettings, workMinutes, breakMinutes } = useContext(PomodoroContext)
+	const secondsLeftRef = useRef(secondsLeft)
+	const isPausedRef = useRef(isPaused)
+	const modeRef = useRef(mode) // Using useRef as setInterval() doesn`t have scope
 
-    const [isPaused, setIsPaused] = useState(true)
-    const [mode, setMode] = useState('work')
-    const [secondsLeft, setSecondsLeft] = useState(0)
-    const [showSettingIcon, setShowSettingIcon] = useState(false)
+	const notifyBreak = () =>
+		toast("Breeeeeak", {
+			icon: "🎉",
+		})
 
-    const secondsLeftRef = useRef(secondsLeft)
-    const isPausedRef = useRef(isPaused)
-    const modeRef = useRef(mode)
-    // I have to create REF because SET INTERVAL doesn`t have SCOPE
+	const notifyWork = () =>
+		toast("Wooork!!", {
+			icon: "💪🏻",
+		})
 
-    const notifyBreak = () =>
-        toast("Breeeeeak", {
-            icon: "🎉",
-        })
+	const switchMode = () => {
+		let nextMode = modeRef.current === "work" ? "break" : "work"
+		let nextSeconds = (nextMode === "work" ? workMinutes : breakMinutes) * 60
 
-    const notifyWork = () =>
-        toast("Wooork!!", {
-            icon: "💪🏻",
-        })
+		modeRef.current = nextMode
+		setMode(nextMode)
 
-    const switchMode = () => {
+		if (nextMode === "work") {
+			notifyWork()
+		} else {
+			notifyBreak()
+		}
 
-        let nextMode = modeRef.current === 'work' ? 'break' : 'work'
-        let nextSeconds = (nextMode === 'work' ? workMinutes : breakMinutes) * 60
+		secondsLeftRef.current = nextSeconds
+		setSecondsLeft(nextSeconds)
+	}
 
-        modeRef.current = nextMode
-        setMode(nextMode)
+	useEffect(() => {
+		secondsLeftRef.current = workMinutes * 60
+		setSecondsLeft(secondsLeftRef.current)
 
-        if (nextMode === 'work') {
-            notifyWork()
-        } else {
-            notifyBreak()
-        }
+		const tick = () => {
+			secondsLeftRef.current--
+			setSecondsLeft(secondsLeftRef.current)
+		}
 
-        secondsLeftRef.current = nextSeconds
-        setSecondsLeft(nextSeconds)
+		const interval = setInterval(() => {
+			if (isPausedRef.current) {
+				return
+			}
+			if (secondsLeftRef.current === 0) {
+				return switchMode()
+			}
+			tick()
+		}, 1000)
 
-    }
+		return () => clearInterval(interval)
+	}, [PomodoroContext])
 
+	const totalSeconds = mode === "work" ? workMinutes * 60 : breakMinutes * 60
+	const percentege = Math.round((secondsLeft / totalSeconds) * 100)
 
-    useEffect(() => {
+	let minutes = Math.floor(secondsLeft / 60)
+	let seconds = secondsLeft % 60
+	if (minutes < 10) {
+		minutes = "0" + minutes
+	}
+	if (seconds < 10) {
+		seconds = "0" + seconds
+	}
 
-        secondsLeftRef.current = workMinutes * 60
-        setSecondsLeft(secondsLeftRef.current)
+	const handleMouseOver = () => {
+		setShowSettingIcon(true)
+	}
 
+	const handleMouseOut = () => {
+		setShowSettingIcon(false)
+	}
 
-        const tick = () => {
+	return (
+		<div className={!darkMode ? "Pomodoro mt-4" : "Pomodoro-dark mt-4"} onMouseOver={() => handleMouseOver()} onMouseOut={() => handleMouseOut()}>
+			<CircularProgressbar
+				value={percentege}
+				strokeWidth={4}
+				styles={buildStyles({
+					textColor: darkMode ? "var(--dark-text-primary)" : "var(--text-primary)",
+					pathColor: mode === "work" ? "var(--color-primary)" : "var(--text-secondary)",
+					trailColor: darkMode ? "var(--bg-modal)" : "var(--text-terciary)",
+					strokeLinecap: "round",
+				})}
+			/>
+			<p>{minutes + ":" + seconds}</p>
 
-            secondsLeftRef.current--
-            setSecondsLeft(secondsLeftRef.current)
-        }
-
-        const interval = setInterval(() => {
-
-            if (isPausedRef.current) {
-                return
-            }
-            if (secondsLeftRef.current === 0) {
-                return switchMode()
-            }
-            tick()
-        }, 1000)
-
-        return () => clearInterval(interval)
-    }, [PomodoroContext])
-
-    const totalSeconds = mode === 'work' ? workMinutes * 60 : breakMinutes * 60
-    const percentege = Math.round(secondsLeft / totalSeconds * 100)
-
-    let minutes = Math.floor(secondsLeft / 60)
-    let seconds = secondsLeft % 60
-    if (minutes < 10) { minutes = '0' + minutes }
-    if (seconds < 10) { seconds = '0' + seconds }
-
-    const handleMouseOver = () => {
-        setShowSettingIcon(true)
-    }
-
-    const handleMouseOut = () => {
-        setShowSettingIcon(false)
-    }
-
-    return (
-
-        <div className={!darkMode ? "Pomodoro mt-4" : "Pomodoro-dark mt-4"}
-            onMouseOver={() => handleMouseOver()}
-            onMouseOut={() => handleMouseOut()}>
-            <CircularProgressbar
-                value={percentege}
-                strokeWidth={4}
-                text={<tspan dy={0} dx={-19} >{minutes + ':' + seconds}</tspan>}
-                styles={buildStyles({
-                    textColor: '#fff',
-                    pathColor: mode === 'work' ? purple : gray,
-                    trailColor: lightGray,
-                    strokeLinecap: 'round',
-                })
-                }
-            />
-
-            {isPaused ? <PlayButton onClick={() => { setIsPaused(false); isPausedRef.current = false }} />
-                : <PauseButton onClick={() => { setIsPaused(true); isPausedRef.current = true }} />}
-            {showSettingIcon && (<SettingsButton onClick={() => setShowSettings(true)} />)}
-        </div>
-    )
+			{isPaused ? (
+				<button
+					onClick={() => {
+						setIsPaused(false)
+						isPausedRef.current = false
+					}}
+					className={!darkMode ? "pomodoroBtn" : "pomodoroBtn-dark"}
+				>
+					<i className="bi bi-play"></i>
+				</button>
+			) : (
+				<button
+					onClick={() => {
+						setIsPaused(true)
+						isPausedRef.current = true
+					}}
+					className={!darkMode ? "pomodoroBtn" : "pomodoroBtn-dark"}
+				>
+					<i className="bi bi-pause"></i>
+				</button>
+			)}
+			{showSettingIcon && (
+				<button onClick={() => setShowSettings(true)} className={!darkMode ? "pomodoroBtn settingsBtn" : "pomodoroBtn-dark settingsBtn-dark"}>
+					<i className="bi bi-gear"></i>
+				</button>
+			)}
+		</div>
+	)
 }
 
 export default Timer
