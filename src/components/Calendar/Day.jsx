ba91874435da681062b01416891f283e71cd6103
Manuel Atance <atance3@hotmail.com>
2023-01-12 23:58:02 +0100

    Added styling to calendar events

--- src/components/Calendar/Day.jsx
+++ src/components/Calendar/Day.jsx
@@ -6,52 +6,57 @@ import CalIndexContext from "../../context/calindex.context"
 import dayjs from "dayjs"
 
 const Day = ({ day, events, setEvents }) => {
+	const [dayEvents, setDayEvents] = useState([])
 
-    const [dayEvents, setDayEvents] = useState([])
+	const { darkMode } = useContext(DarkModeContext)
+	const { setDaySelected, setEventModal, setSelectedEvent, eventId, setEventId } = useContext(CalIndexContext)
 
+	useEffect(() => {
+		const eventsFiltered = events.filter(event => dayjs(event.startDate).format("DD-MM-YY") === day.format("DD-MM-YY"))
+		setDayEvents(eventsFiltered)
+	}, [events, day])
 
-    const { darkMode } = useContext(DarkModeContext)
-    const { setDaySelected, setEventModal, setSelectedEvent, eventId, setEventId } = useContext(CalIndexContext)
+	const getCurrentDayClass = () => {
+		return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY") ? "currentDay text-white" : "otherDay"
+	}
 
-    useEffect(() => {
-        const eventsFiltered = events.filter((event) =>
-            dayjs(event.startDate).format("DD-MM-YY") === day.format("DD-MM-YY")
-        )
-        setDayEvents(eventsFiltered)
+	const handleMouseOver = id => {
+		setEventId(id)
+	}
 
-    }, [events, day])
+	const handleMouseOut = () => {
+		setEventId("")
+	}
 
-    const getCurrentDayClass = () => {
-        return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY") ? "currentDay text-white" : "otherDay"
-    }
+	console.log(eventId)
 
-    const handleMouseOver = id => {
-        setEventId(id)
-    }
-
-    const handleMouseOut = () => {
-        setEventId("")
-    }
-
-    console.log(eventId)
-
-    return (
-        <div onClick={() => {
-            setDaySelected(day)
-            setEventModal(true)
-        }} className={!darkMode ? "date" : "date-dark"}>
-            <p className={`${getCurrentDayClass()}`}>{day.format("DD")} </p>
-            {dayEvents.map((event, idx) => {
-                return (
-                    <div /* onMouseOver={() => handleMouseOver(event._id)} onMouseOut={handleMouseOut}  */
-                        onClick={() => {
-                            setSelectedEvent(event)
-                            setEventId(event.startDate)
-                        }} key={idx}>{event.title}</div>
-                )
-            })}
-        </div>
-    )
+	return (
+		<div
+			onClick={() => {
+				setDaySelected(day)
+				setEventModal(true)
+			}}
+			className={!darkMode ? "date" : "date-dark"}
+		>
+			<p className={`${getCurrentDayClass()}`}>{day.format("DD")} </p>
+			{dayEvents.map((event, idx) => {
+				return (
+					<div style={{ height: "30px", maxHeight: "30px" }} /* onMouseOver={() => handleMouseOver(event._id)} onMouseOut={handleMouseOut}  */>
+						<p
+							onClick={() => {
+								setSelectedEvent(event)
+								setEventId(event.startDate)
+							}}
+							key={idx}
+							className={!darkMode ? `eventLine ${event.tag}Category fontLight` : `eventLine-dark ${event.tag}CategoryDark fontDark`}
+						>
+							{event.title}
+						</p>
+					</div>
+				)
+			})}
+		</div>
+	)
 }
 
 export default Day
--- src/components/Calendar/Month.jsx
+++ src/components/Calendar/Month.jsx
@@ -6,25 +6,25 @@ import CalIndexContext from "../../context/calindex.context"
 import Day from "./Day"
 
 const Month = ({ currentMonth, events, setEvents }) => {
-
 	const { darkMode } = useContext(DarkModeContext)
 	const { setDaySelected, setEventModal } = useContext(CalIndexContext)
-
 
 	return (
 		<>
 			<div className="weekDays mt-4">
 				{currentMonth[0].map((day, idx) => {
-					return (
-						<p key={idx}>{day.format("ddd").toUpperCase()}</p>
-					)
+					return <p key={idx}>{day.format("ddd").toUpperCase()}</p>
 				})}
 			</div>
 			<div className="month">
 				{currentMonth.map((row, idx) => {
 					return (
 						<div key={idx}>
-							{row.map((day, idx) => <div key={idx}><Day events={events} setEvents={setEvents} day={day} /></div>)}
+							{row.map((day, idx) => (
+								<div key={idx}>
+									<Day events={events} setEvents={setEvents} day={day} />
+								</div>
+							))}
 						</div>
 					)
 				})}
